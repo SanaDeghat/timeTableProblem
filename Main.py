d00b34f51055d99950e4025e5884b760eb852919
Veronica <verkon168@gmail.com>
2026-05-20 13:25:57 -0700

    added getFile function

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
+import pandas as pd
+
 from Student import Student 
 from Class import Class 
 
 S = Student(1234)
 S.print()
 
-print("Hello World")
+# returns a 2D labeled table with rows and columns
+def getFile(fileName):
+    df = pd.read_csv(fileName + '.csv')
+
+    return df
