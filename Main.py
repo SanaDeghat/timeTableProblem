18e885069e4dc9fb7391e85190f97a329f4a4ec7
Veronica <verkon168@gmail.com>
2026-05-21 14:49:04 -0700

    add comments in main

--- Main.py
+++ Main.py
@@ -5,15 +5,19 @@ from Class import Class
 
 def main():
 
+    # dict of all students in this format: {"id" : Student Obj}
     students = {}
     fileName = "Course Selection by student"
 
+    # opens data file
     with open(f"DataFiles/{fileName}.csv", mode='r') as file:
         data = csv.reader(file)
         next(data, None)
 
+        # loops through all the student data
         for stu in data:
             id = stu[0]
+
             students[id] = Student(id, stu[1])
         
     for s in students:
