9173e9858c153eb94dcde4964685efcf39fa237e
Danny Zhai <yhdannyzhai2008@gmail.com>
2026-05-21 14:08:13 -0700

    Modified: Student.py

--- Student.py
+++ Student.py
@@ -1,7 +1,8 @@
 class Student:
-    def __init__(self, id, courses):
+    def __init__(self, id, courses, yog):
         self.id = id
         self.courses = courses
-        
+        self.yog = yog
+
     def print(self):
         print(self.id)
