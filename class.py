1948e824ace8306660b47dddf1341cb357bf3f2b
Danny Zhai <yhdannyzhai2008@gmail.com>
2026-05-21 14:35:27 -0700

    Update class file

--- class.py
+++ class.py
@@ -1,8 +1,10 @@
 class Class:
-    def __init__(self, name, roomNumber, numOfStudents):
+    def __init__(self, name, roomNumber, numOfStudents, block, sem):
         self.name = name
         self.roomNumber = roomNumber
         self.numOfStudents = numOfStudents
+        self.block = block
+        self.sem = sem
 
     def print(self):
-        print(self.name, self.roomNumber, self.numOfStudents)
+        print(self.name, self.roomNumber, self.numOfStudents, self.block, self.sem)
